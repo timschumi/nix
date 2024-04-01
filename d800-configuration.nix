3f36946fe2266e78e1115282cb66a59f323afae5
Tim Schumacher <timschumi@gmx.de>
2024-04-02 01:07:11 +0200

    Import the configuration for d800

---
+++ d800-configuration.nix
@@ -0,0 +1,39 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./d800-hardware-configuration.nix
+    ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+
+  networking.hostName = "d800";
+  networking.wireless.enable = true;
+
+  time.timeZone = "Europe/Berlin";
+
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "de-latin1-nodeadkeys";
+  };
+
+  users.users.tim = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    packages = with pkgs; [
+      curl
+      tree
+      vim
+      wget
+    ];
+  };
+
+  services.openssh.enable = true;
+
+  system.stateVersion = "23.11";
+
+}
+
+
--- flake.nix
+++ flake.nix
@@ -6,6 +6,13 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.d800 = nixpkgs.lib.nixosSystem {
+      system = "i686-linux";
+      modules = [
+        ./d800-configuration.nix
+      ];
+    };
+
     nixosConfigurations.m600 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
