2056d6add1e9947a600d6efbe033275d48ce9136
Tim Schumacher <timschumi@gmx.de>
2024-06-20 17:38:50 +0200

    modules/variant-base: Enable more locales

--- modules/variant-base.nix
+++ modules/variant-base.nix
@@ -13,6 +13,12 @@
   time.timeZone = "Europe/Berlin";
 
   i18n.defaultLocale = "de_DE.UTF-8";
+  i18n.supportedLocales = [
+    "C.UTF-8/UTF-8"
+    "en_US.UTF-8/UTF-8"
+    "de_DE.UTF-8/UTF-8"
+    "ja_JP.UTF-8/UTF-8"
+  ];
   console = {
     font = "Lat2-Terminus16";
     keyMap = "de-latin1-nodeadkeys";
