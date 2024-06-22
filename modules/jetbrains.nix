dff26475b2f6c9fbd1b1cd29cf821932ff8a4182
Tim Schumacher <timschumi@gmx.de>
2024-06-22 13:43:06 +0200

    modules/jetbrains: Install other necessary dependencies

--- modules/jetbrains.nix
+++ modules/jetbrains.nix
@@ -6,11 +6,14 @@
   home-manager.users.tim = {
     home.packages = with pkgs.jetbrains; [
       clion
+      pkgs.clang
+      pkgs.pkg-config
       idea-ultimate
       phpstorm
       pycharm-professional
       rider
       rust-rover
+      pkgs.rustup
     ];
   };
 }
