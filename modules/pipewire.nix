f12ec93e75a1584f715c6a22393b37bbee81b70b
Tim Schumacher <timschumi@gmx.de>
2024-07-01 22:30:03 +0200

    modules/pipewire: Open a network socket for incoming audio data

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -4,4 +4,18 @@
   services.pipewire.alsa.enable = true;
   services.pipewire.alsa.support32Bit = true;
   services.pipewire.pulse.enable = true;
+
+  services.pipewire.extraConfig.pipewire-pulse."90-network" = {
+    "context.modules" = [
+      {
+        "name" = "libpipewire-module-protocol-simple";
+        "args" = {
+          "playback" = true;
+          "server.address" = [
+            "tcp:27459"
+          ];
+        };
+      }
+    ];
+  };
 }
