a47575263e0c1881ef1d55805da31104508ff048
Tim Schumacher <timschumi@gmx.de>
2024-07-02 07:38:19 +0200

    modules/adb: Install heimdall while at it

--- modules/adb.nix
+++ modules/adb.nix
@@ -5,4 +5,18 @@
 }: {
   programs.adb.enable = true;
   users.users.tim.extraGroups = ["adbusers"];
+
+  home-manager.users.tim = {
+    home.packages = with pkgs; [
+      (heimdall-gui.overrideAttrs (final: prev: rec {
+        version = "2.0.2";
+        src = pkgs.fetchFromSourcehut {
+          owner = "~grimler";
+          repo = "Heimdall";
+          rev = "v${version}";
+          sha256 = "sha256-tcAE83CEHXCvHSn/S9pWu6pUiqGmukMifEadqPDTkQ0=";
+        };
+      }))
+    ];
+  };
 }
