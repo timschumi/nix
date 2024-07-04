b62bb68bd3d99300b0c2e2efba192a1c5d766e9e
Tim Schumacher <timschumi@gmx.de>
2024-07-04 04:38:30 +0200

    hosts/b550: Enable GPU acceleration

--- hosts/nixos/b550.nix
+++ hosts/nixos/b550.nix
@@ -58,6 +58,18 @@
         boot.loader.efi.canTouchEfiVariables = true;
 
         networking.hostName = "b550";
+
+        hardware.graphics = {
+          enable = true;
+          enable32Bit = true;
+          extraPackages = with pkgs; [
+            rocmPackages.clr.icd
+            amdvlk
+          ];
+          extraPackages32 = with pkgs.pkgsi686Linux; [
+            amdvlk
+          ];
+        };
       }
     )
   ];
