b50f04d09b0abd5a0eb583ca49650c7c7373ec57
Tim Schumacher <timschumi@gmx.de>
2024-07-28 20:43:25 +0200

    hosts/b550: Disable amdvlk
    This causes issues with a subset of games due to unimplemented features

--- hosts/nixos/b550.nix
+++ hosts/nixos/b550.nix
@@ -68,10 +68,8 @@
           enable32Bit = true;
           extraPackages = with pkgs; [
             rocmPackages.clr.icd
-            amdvlk
           ];
           extraPackages32 = with pkgs.pkgsi686Linux; [
-            amdvlk
           ];
         };
       }
