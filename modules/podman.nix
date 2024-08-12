251cec15eb18867960c7299fa4e5b18fc6bf3c6b
Tim Schumacher <timschumi@gmx.de>
2024-08-12 23:16:21 +0200

    modules/podman: Install podman-compose

--- modules/podman.nix
+++ modules/podman.nix
@@ -16,4 +16,9 @@
   # The setting linked to dns_enabled is not enough for non-default networks.
   networking.firewall.interfaces."podman+".allowedUDPPorts = [53 5353];
   networking.firewall.interfaces."podman+".allowedTCPPorts = [53];
+
+  # Provide a compose implementation for `podman compose`.
+  environment.systemPackages = with pkgs; [
+    podman-compose
+  ];
 }
