b0f7092c33487a21e96e15bde7b2d80a758701f1
Tim Schumacher <timschumi@gmx.de>
2024-08-15 14:01:44 +0200

    modules/firefox: Install a standard selection of extensions

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nur.url = github:nix-community/NUR;
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
--- modules/firefox.nix
+++ modules/firefox.nix
@@ -1,10 +1,21 @@
 {
+  config,
   pkgs,
   inputs,
   ...
 }: {
+  imports = [
+    inputs.nur.nixosModules.nur
+  ];
+
   home-manager.users.tim = {
     programs.firefox.enable = true;
     programs.firefox.profiles.main.isDefault = true;
+    programs.firefox.profiles.main.extensions = with config.nur.repos.rycee.firefox-addons; [
+      keepassxc-browser
+      reddit-enhancement-suite
+      sponsorblock
+      ublock-origin
+    ];
   };
 }
