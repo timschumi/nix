c4f8298730981a5732454dcb318bace32112ab6f
Tim Schumacher <timschumi@gmx.de>
2024-09-02 00:00:46 +0200

    hosts/b450: Initial configuration

---
+++ hosts/nixos/b450.nix
@@ -0,0 +1,61 @@
+{inputs, ...}: {
+  system = "x86_64-linux";
+  modules = [
+    (inputs.self + "/fragments/comma.nix")
+    (import (inputs.self + "/fragments/emulate.nix") [
+      "aarch64-linux"
+    ])
+    (inputs.self + "/fragments/home.nix")
+    (inputs.self + "/fragments/variant-desktop.nix")
+
+    (
+      {
+        config,
+        lib,
+        pkgs,
+        modulesPath,
+        ...
+      }: {
+        imports = [
+          (modulesPath + "/installer/scan/not-detected.nix")
+        ];
+
+        boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod"];
+        boot.initrd.kernelModules = [];
+        boot.kernelModules = ["kvm-amd"];
+        boot.extraModulePackages = [];
+
+        fileSystems."/" = {
+          device = "/dev/disk/by-uuid/79b2478c-5fd1-4bc2-860b-587488f2b61d";
+          fsType = "ext4";
+        };
+
+        fileSystems."/boot" = {
+          device = "/dev/disk/by-uuid/85A5-F011";
+          fsType = "vfat";
+          options = ["fmask=0077" "dmask=0077"];
+        };
+
+        swapDevices = [];
+
+        nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+        hardware.cpu.amd.updateMicrocode = true;
+
+        boot.loader.systemd-boot.enable = true;
+        boot.loader.efi.canTouchEfiVariables = true;
+
+        networking.hostName = "b450";
+
+        hardware.graphics = {
+          enable = true;
+          enable32Bit = true;
+          extraPackages = with pkgs; [
+            rocmPackages.clr.icd
+          ];
+          extraPackages32 = with pkgs.pkgsi686Linux; [
+          ];
+        };
+      }
+    )
+  ];
+}
