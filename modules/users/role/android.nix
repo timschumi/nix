d22268fedb2e930022c16af6bc300ce5557959fe
Tim Schumacher <timschumi@gmx.de>
2024-10-04 18:27:05 +0200

    modules/android: Update heimdall to v2.1.0

--- modules/users/role/android.nix
+++ modules/users/role/android.nix
@@ -8,8 +8,9 @@
   pkgs,
   ...
 }: let
-  inherit (builtins) elem;
+  inherit (builtins) elem filter;
   inherit (inputs.nixpkgs.lib) mkIf;
+  inherit (inputs.nixpkgs.lib.strings) concatLines hasInfix splitString;
 in {
   config = mkIf (elem role config.extra.user."${user}".roles) {
     programs.adb.enable = true;
@@ -23,13 +24,14 @@ in {
         brotli
         bsdiff
         (heimdall-gui.overrideAttrs (final: prev: rec {
-          version = "2.0.2";
+          version = "2.1.0";
           src = pkgs.fetchFromSourcehut {
             owner = "~grimler";
             repo = "Heimdall";
             rev = "v${version}";
-            sha256 = "sha256-tcAE83CEHXCvHSn/S9pWu6pUiqGmukMifEadqPDTkQ0=";
+            sha256 = "sha256-4tOUVwcUGAEpZR+2QntYSaphUQUjXUbJCpZIQoHuLog=";
           };
+          installPhase = concatLines (filter (line: !(hasInfix "share/doc/heimdall/" line)) (splitString "\n" prev.installPhase));
         }))
         scrcpy
         sdat2img
