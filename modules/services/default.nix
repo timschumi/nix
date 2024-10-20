028f54098bbe38905a2fa0170e30dcc45653c7ff
Tim Schumacher <timschumi@gmx.de>
2024-10-20 20:38:47 +0200

    services/watchtower: Add multiple scope-based watchtower instances

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -1,6 +1,5 @@
 {...}: {
   imports = [
-    ./services
-    ./users
+    ./watchtower.nix
   ];
 }
---
+++ modules/services/watchtower.nix
@@ -0,0 +1,60 @@
+{
+  config,
+  inputs,
+  ...
+}: let
+  inherit (builtins) attrNames;
+  inherit (inputs.nixpkgs.lib) mkIf;
+  inherit (inputs.nixpkgs.lib.attrsets) genAttrs;
+  inherit (inputs.nixpkgs.lib.options) mkEnableOption mkOption;
+  inherit (inputs.nixpkgs.lib.types) attrsOf submodule;
+in {
+  options.extra.services.watchtower = {
+    enable = mkEnableOption "Enable the `watchtower` service";
+  };
+
+  # HACK: Force adding a default label without getting infinite recursion.
+  options = {
+    virtualisation.oci-containers.containers = mkOption {
+      type = attrsOf (submodule {
+        config.labels = {
+          "com.centurylinklabs.watchtower.scope" = "nix";
+        };
+      });
+    };
+  };
+
+  config = mkIf config.extra.services.watchtower.enable {
+    virtualisation.oci-containers.containers."watchtower-nix" = {
+      image = "docker.io/containrrr/watchtower:latest";
+      volumes = [
+        "/var/run/docker.sock:/var/run/docker.sock:rw"
+      ];
+      log-driver = "journald";
+      cmd = [
+        "--scope"
+        "nix"
+        # FIXME: Link this into the systemd container management.
+        "--monitor-only"
+        "--cleanup"
+        "--interval"
+        "300"
+      ];
+    };
+
+    virtualisation.oci-containers.containers."watchtower-unscoped" = {
+      image = "docker.io/containrrr/watchtower:latest";
+      volumes = [
+        "/var/run/docker.sock:/var/run/docker.sock:rw"
+      ];
+      log-driver = "journald";
+      cmd = [
+        "--scope"
+        "none"
+        "--cleanup"
+        "--interval"
+        "300"
+      ];
+    };
+  };
+}
