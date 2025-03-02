0bfc6a51912f7cb087e4e0eff34faac7aef3dc4c
Tim Schumacher <timschumi@gmx.de>
2025-03-02 21:01:39 +0100

    secrets: Remove an outdated agenix identity

--- modules/secrets/default.nix
+++ modules/secrets/default.nix
@@ -15,10 +15,6 @@
         identity = inputs.self + "/secrets/yubikey.pub";
         pubkey = "age1yubikey1qff5k54lavr7nx7m93sccygfcek7l9x8899mnq09nq6ksqdgtuaa5tj2v7y";
       }
-      {
-        identity = "/home/tim/.ssh/id_ed25519_agenix";
-        pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC/B9a7FBpPLWytP9GHSVqF64DexQTEdAEsMZ4UXfxZ6";
-      }
     ];
     storageMode = "local";
     localStorageDir = inputs.self.outPath + "/secrets/rekeyed/${config.networking.hostName}";
